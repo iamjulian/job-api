574552309e248d49171a5795f89b91ead90a9b1c
JULIAN ANTHONY <86510806+iamjulian@users.noreply.github.com>
2024-08-23 16:59:21 +0530

    Add files via upload

---
+++ services/user.services.js
@@ -0,0 +1,31 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
+const signUpServices = async (data) => {
+  console.log("data",data);
+  console.log("password",data.password);
+  //   const password = this.password;
+
+  //   const hashedPassword = bcrypt.hashSync(password);
+
+  //   this.password = hashedPassword;
+  //   this.confirmPassword = undefined;
+  if (data.password !== data.confirmPassword) {
+    return "confirmPassword Passwords don't match!";
+  }
+  const password = data.password;
+  const hashedPassword = bcrypt.hashSync(password);
+  data.password = hashedPassword;
+  const user = await User.create(data);
+  return user;
+};
+
+const verifyEmailServices = async (token) => {
+  return await User.findOne({ confirmationToken: token });
+};
+
+const findUserByEmail = async (email) => {
+  return await User.findOne({ email });
+};
+
+module.exports = { signUpServices, verifyEmailServices, findUserByEmail };
