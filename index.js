7d3a8d79553f7162bffbc0da6353a7bc737e5f49
iamjulian <julian.anthony4@gmail.com>
2024-09-02 05:05:35 +0530

    added local db connection

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ const dotenv = require("dotenv").config();
 const colors = require("colors");
 const app = require("./app");
 
-// database connection
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@job-portal.owls5.mongodb.net/`;
+// hosted database connection
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@job-portal.owls5.mongodb.net/`;
+// mongoose.connect(uri).then(() => {
+//   console.log(`Successfully connected to database`.blue.bold);
+// });
 
-mongoose.connect(uri).then(() => {
-  console.log(`Successfully connected to database`.blue.bold);
-});
+//Local database connection
+const localUri = `mongodb://localhost:27017/${process.env.TOKEN_KEY}`;
+mongoose.connect(localUri,{useNewUrlParser: true});
+const db = mongoose.connection;
+db.on('error',console.error.bind(console, 'database connection error:'));
+db.once('open', () => {console.log(`Successfully connected to database`.blue.bold);});
 
 // server
 const port = process.env.PORT || 5000;
